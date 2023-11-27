dcadcbe83815d852a737fb42ed4c55028a0f6a75
arusk <arusk@Ubuntu20.04.myguest.virtualbox.org>
2023-11-27 14:41:02 -0800

    Add files from tutorial

---
+++ catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/DependInfo.cmake
@@ -0,0 +1,73 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/audio.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/audio.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/camera.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/camera.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/diagnostics.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/diagnostics.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/imu.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/imu.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/info.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/info.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/joint_state.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/joint_state.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/laser.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/laser.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/log.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/log.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/memory/bool.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/memory/bool.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/memory/float.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/memory/float.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/memory/int.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/memory/int.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/memory/string.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/memory/string.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/memory_list.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/memory_list.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/odom.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/odom.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/sonar.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/sonar.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/converters/touch.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/converters/touch.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/event/audio.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/event/audio.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/event/touch.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/event/touch.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/helpers/driver_helpers.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/helpers/driver_helpers.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/naoqi_driver.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/naoqi_driver.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/publishers/camera.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/publishers/camera.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/publishers/info.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/publishers/info.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/publishers/joint_state.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/publishers/joint_state.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/publishers/log.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/publishers/log.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/publishers/sonar.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/publishers/sonar.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/recorder/camera.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/recorder/camera.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/recorder/diagnostics.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/recorder/diagnostics.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/recorder/globalrecorder.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/recorder/globalrecorder.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/recorder/joint_state.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/recorder/joint_state.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/recorder/log.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/recorder/log.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/recorder/sonar.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/recorder/sonar.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/services/get_language.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/services/get_language.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/services/robot_config.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/services/robot_config.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/services/set_language.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/services/set_language.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/subscribers/moveto.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/subscribers/moveto.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/subscribers/speech.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/subscribers/speech.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/subscribers/teleop.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/subscribers/teleop.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/tools/from_any_value.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/tools/from_any_value.cpp.o"
+  "/home/arusk/catkin_ws/src/naoqi_driver/src/tools/robot_description.cpp" "/home/arusk/catkin_ws/build/naoqi_driver/CMakeFiles/naoqi_driver.dir/src/tools/robot_description.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CATKIN_BUILD"
+  "LIBQI_VERSION=29"
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"naoqi_driver\""
+  "naoqi_driver_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/arusk/catkin_ws/src/naoqi_driver/include"
+  "/opt/ros/noetic/include"
+  "/opt/ros/noetic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  "/usr/include/eigen3"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
